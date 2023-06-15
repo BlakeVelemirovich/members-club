0c8265bb09b6f96ee57290eda98749142ee9f549
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-06-15 18:24:20 -0300

    Models for users and posts created

--- models/user.js
+++ models/user.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: { type: String, required: true, maxLength: 20 },
+    last_name: { type: String, required: true, maxLength: 20},
+    username: { type: String, required: true, maxLength: 20},
+    password: { type: String, required: true, maxLength: 30},
+    membership: { type: Boolean, required: true},
+    admin: { type: Boolean, required: true}
+});
+
+module.exports = mongoose.model("User", UserSchema);
